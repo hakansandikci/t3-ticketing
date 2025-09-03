498341b9d5ffff58a39360b806214c8a78685e4d
Haluk Şakir Ekinci <hseekinci@gmail.com>
2025-09-03 15:55:57 +0300

    build

--- render_build.sh
+++ render_build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+set -e  # hata olursa scripti durdurur
+
+# Gereksinimleri yükle
 pip install -r requirements.txt
+
+# Migrasyonları uygula
 python manage.py migrate
+
+# Statik dosyaları topla
 python manage.py collectstatic --noinput
 
+# Superuser oluştur (varsa hata vermesin diye || true ekleniyor)
+DJANGO_SUPERUSER_USERNAME=admin \
+DJANGO_SUPERUSER_PASSWORD=admin123 \
+DJANGO_SUPERUSER_EMAIL=admin@example.com \
+python manage.py createsuperuser --noinput || true
